c33010451ff70b8cf1b6a5fd482fa0e253cf1b2e
eugenyberkut <eugeny.berkunsky@gmail.com>
2015-09-15 14:11:06 +0300

    graphic sinus

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -17,24 +17,51 @@ public class Controller {
     private int toScreenX(double x) {
         return (int) (canvas.getWidth() * (x - minX) / (maxX - minX));
     }
+
     private int toScreenY(double y) {
         return (int) (canvas.getHeight() * (1 - (y - minY) / (maxY - minY)));
     }
+
     private double toWorldX(int xs) {
         return 1.0 * xs / canvas.getWidth() * (maxX - minX) + minX;
     }
+
     private double toWorldY(int ys) {
         return (1.0 * ys - canvas.getHeight()) /
                 (-canvas.getHeight()) * (maxY - minY) + minY;
     }
 
     public void drawGraphics(ActionEvent actionEvent) {
-        double x1 = -4, y1 = -4, x2 = 4, y2 = 4;
-        int xs1 = toScreenX(x1);
-        int xs2 = toScreenX(x2);
-        int ys1 = toScreenY(y1);
-        int ys2 = toScreenY(y2);
+//        double x1 = -4, y1 = -4, x2 = 4, y2 = 4;
+//        int xs1 = toScreenX(x1);
+//        int xs2 = toScreenX(x2);
+//        int ys1 = toScreenY(y1);
+//        int ys2 = toScreenY(y2);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.strokeLine(xs1,ys1,xs2,ys2);
+        tabul();
+//        gc.strokeLine(xs1,ys1,xs2,ys2);
+        for (int i = 0; i < x.length - 1; i++) {
+            int xs1 = toScreenX(x[i]);
+            int xs2 = toScreenX(x[i + 1]);
+            int ys1 = toScreenY(y[i]);
+            int ys2 = toScreenY(y[i + 1]);
+            gc.strokeLine(xs1,ys1,xs2,ys2);
+        }
+        gc.strokeLine(toScreenX(0), toScreenY(minY), toScreenX(0), toScreenY(maxY));
+        gc.strokeLine(toScreenX(minX), toScreenY(0), toScreenX(maxX), toScreenY(0));
     }
+
+    private void tabul() {
+        for (int i = 0; i < 500; i++) {
+            x[i] = toWorldX(i);
+            y[i] = f(x[i]);
+        }
+    }
+
+    double f(double x) {
+        return Math.sin(x);
+    }
+
+    double[] x = new double[500];
+    double[] y = new double[500];
 }
